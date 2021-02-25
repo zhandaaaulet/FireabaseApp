a96947da85e16e3a380b4492a79179fe5f95b560
zhandaaaulet <zh.myrzatayev@gmail.com>
2021-02-25 13:51:06 +0600

    fixed bugs

--- app/src/main/java/com/example/introtokotlinfirebase/MainActivity.kt
+++ app/src/main/java/com/example/introtokotlinfirebase/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.introtokotlinfirebase
+package com.company.introtokotlinfirebase
 
-import android.app.Person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.example.introtokotlinfirebase.R
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -13,68 +15,89 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import java.text.FieldPosition
 
-class MainActivity : AppCompatActivity() {
-    private var mAuth: FirebaseAuth? = null
+class MainActivity2 : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null;
     private var currentUser: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var email: EditText? = null
+        var password: EditText? = null;
+        var button: Button = findViewById(R.id.button)
+
         val database = FirebaseDatabase.getInstance()
-        val databaseRef = database.getReference("message").push()
+        val myRef = database.getReference("message")
 
         mAuth = FirebaseAuth.getInstance()
 
-        mAuth!!.signInWithEmailAndPassword("paulo@me.com", "password")
-            .addOnCompleteListener { task: Task<AuthResult> ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        this, "Signed in Succesful",
-                        Toast.LENGTH_LONG
-                    ).show()
-                } else {
-                    Toast.makeText(
-                        this, "Signed in Unsuccesful",
-                        Toast.LENGTH_LONG
-                    ).show()
+        email = findViewById(R.id.emailID)
+        password = findViewById(R.id.passwordID)
+
+
+
+        button.setOnClickListener{
+            var email1 = email.text.toString()
+            var pass = password.text.toString()
+            mAuth!!.createUserWithEmailAndPassword(email1, pass)
+                .addOnCompleteListener(this) { task: Task<AuthResult> ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "User:" + email1, Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
+                    }
                 }
-            }
+        }
+
+
+
+
+
+       /* mAuth!!.signInWithEmailAndPassword("naruto", "123")
+            .addOnCompleteListener {
+                    task: Task<AuthResult> ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, "Signed In Successful", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this, "Signed In Unsuccessful", Toast.LENGTH_LONG).show()
+                }
+            }*/
+
 //        var James = Person("James", 30)
-        var employee = Employee(
-            "Bonni D", "Professor ",
-            "123 South Street", 25
+        /*var employee = Employee(
+            "James Bond", "Android developer?XD",
+            "123 South Street", 51
         )
-        //databaseRef.setValue(employee)
+        myRef.setValue(employee)*/
 
 
 //        myRef.setValue("Hello, From Android x Kotlin!")
 
-        databaseRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value = dataSnapshot!!.value as HashMap<String, Any>
-//                Log.d("VALUE: ", value.toString())
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value as HashMap<String, Any>
+                Log.d("VALUE: ", value.toString())
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.d("ERROR: ", error!!.message)
+                Log.d("ERROR: ", error.message)
             }
+
         })
-
-
     }
 
-    override fun onStart() {
+    /*override fun onStart() {
         super.onStart()
-
         currentUser = mAuth!!.currentUser
-        if (currentUser != null) {
-            Toast.makeText(this, "User is logged in", Toast.LENGTH_LONG).show()
+        if(currentUser != null){
+            Toast.makeText(this, "User is loggged in", Toast.LENGTH_LONG).show()
         }
-
-    }
-
+        else{
+            Toast.makeText(this, "User is loggged out", Toast.LENGTH_LONG).show()
+        }
+    }*/
 
     class Employee() {
         var name: String? = null
@@ -82,12 +105,11 @@ class MainActivity : AppCompatActivity() {
         var homeAddress: String? = null
         var age: Int? = null
 
-        constructor(name: String, position: String, homeAdress: String, age: Int) : this() {
+        constructor(name: String, position: String, homeAddress: String, age: Int): this(){
             this.name = name
             this.position = position
             this.homeAddress = homeAddress
             this.age = age
         }
-
     }
 }
